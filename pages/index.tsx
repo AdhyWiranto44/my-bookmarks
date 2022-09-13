e82e6af2af40ea08997677f79a89762894bf0095
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-09-13 15:54:41 +0700

    feat: category selector

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,27 @@
-import Navbar from '../components/navbar'
-import Searchbar from '../components/Searchbar'
+import Navbar from "../components/navbar";
+import Searchbar from "../components/Searchbar";
 
 export default function Home() {
   return (
-    <div className='bg-gray-100'>
+    <>
       <Navbar headingText="My Bookmarks" />
       <div className="px-4 lg:w-6/12 mx-auto mt-8 mb-4">
         <div className="text-center mb-3">
-          <p className='font-bold text-3xl'>Find Now</p>
-          <p className='font-semibold text-lg'><span className='text-gray-400'>in</span> My Bookmarks</p>
+          <p className="font-bold text-3xl">Find Now</p>
+          <p className="font-semibold text-lg">
+            <span className="text-gray-400">in</span> My Bookmarks
+          </p>
         </div>
         <Searchbar placeholder="Find by description" />
       </div>
-    </div>
-  )
+      <div className="px-4 lg:px-44">
+        <select className="text-violet-500 font-bold bg-transparent mt-4 cursor-pointer">
+          <option value="">Choose category</option>
+          <option value="library">Library</option>
+          <option value="tool">Tool</option>
+          <option value="framework">Framework</option>
+        </select>
+      </div>
+    </>
+  );
 }
