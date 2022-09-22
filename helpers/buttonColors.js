42f05a090dec693607d0d3f5b3554fbbe7586c01
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-09-22 16:52:42 +0700

    feat: new button colors

--- helpers/buttonColors.js
+++ helpers/buttonColors.js
@@ -1,4 +1,5 @@
 const buttonColors = {
+  blue: "text-white font-bold bg-blue-500 border border-blue-700 duration-300 hover:bg-blue-600 active:bg-blue-700",
   purple: "text-white font-bold bg-violet-500 border border-violet-700 duration-300 hover:bg-violet-600 active:bg-violet-700",
   yellow: "text-gray-700 font-bold bg-yellow-400 border border-yellow-600 duration-300 hover:bg-yellow-500 active:bg-yellow-600",
   red: "text-white font-bold bg-red-400 border border-red-600 duration-300 hover:bg-red-500 active:bg-red-600",
