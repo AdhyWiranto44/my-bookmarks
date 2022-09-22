825c1278eb5e30f12c83b1872fb3cb4631f9adf0
Adhy Wiranto <30892092+AdhyWiranto44@users.noreply.github.com>
2022-09-22 16:43:54 +0700

    feat: navbar sticky top

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,18 +6,16 @@ import Searchbar from "../components/Searchbar";
 export default function Home() {
   return (
     <>
-      <div className="bg-gray-100 bg-opacity-70 backdrop-blur-sm sticky top-0 z-10">
-        <Navbar headingText="My Bookmarks" />
-        <div className="px-4 lg:w-6/12 mx-auto mt-2 mb-2 pb-2">
-          <div className="text-center mb-3">
-            <p className="font-bold text-3xl text-gray-900">Find Now</p>
-            <p className="font-semibold text-lg text-gray-900">
-              <span className="text-gray-400">in</span> My Bookmarks
-            </p>
-          </div>
-          <Searchbar placeholder="Find by description" />
-          <Filter />
+      <Navbar headingText="My Bookmarks" />
+      <div className="px-4 lg:w-6/12 mx-auto mt-2 mb-2 pb-2">
+        <div className="text-center mb-3">
+          <p className="font-bold text-3xl text-gray-900">Find Now</p>
+          <p className="font-semibold text-lg text-gray-900">
+            <span className="text-gray-400">in</span> My Bookmarks
+          </p>
         </div>
+        <Searchbar placeholder="Find by description" />
+        <Filter />
       </div>
       <div className="px-4 lg:px-44">
         <Bookmarks />
